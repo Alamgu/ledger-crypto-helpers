091c2195a76e98341364b2f7f3837026ecf8b050
John Ericson <John.Ericson@Obsidian.Systems>
2022-09-23 23:41:35 +0000

    Generalize the error type of `with_public_keys`

--- src/eddsa.rs
+++ src/eddsa.rs
@@ -19,14 +19,16 @@ pub fn eddsa_sign(
     Ok(EdDSASignature(sig.0))
 }
 
-pub fn with_public_keys<V, A:Address<A, Ed25519PublicKey>>(
+pub fn with_public_keys<V, E, A:Address<A, Ed25519PublicKey>>(
   path: &[u32],
-  f: impl FnOnce(&nanos_sdk::ecc::ECPublicKey<65, 'E'>, &A) -> Result<V, CryptographyError>
-) -> Result<V, CryptographyError> {
-    let mut pubkey = Ed25519::from_bip32(path).public_key()?;
-    call_c_api_function!(cx_edwards_compress_point_no_throw(CX_CURVE_Ed25519, pubkey.pubkey.as_mut_ptr(), pubkey.keylength as u32))?;
+  f: impl FnOnce(&nanos_sdk::ecc::ECPublicKey<65, 'E'>, &A) -> Result<V, E>
+) -> Result<V, E>
+  where E: From<CryptographyError>
+{
+    let mut pubkey = Ed25519::from_bip32(path).public_key().map_err(Into::<CryptographyError>::into)?;
+    call_c_api_function!(cx_edwards_compress_point_no_throw(CX_CURVE_Ed25519, pubkey.pubkey.as_mut_ptr(), pubkey.keylength as u32)).map_err(Into::<CryptographyError>::into)?;
     pubkey.keylength = 33;
-    let pkh = <A as Address<A, Ed25519PublicKey>>::get_address(&pubkey)?;
+    let pkh = <A as Address<A, Ed25519PublicKey>>::get_address(&pubkey).map_err(Into::<CryptographyError>::into)?;
     f(&pubkey, &pkh)
 }
 
